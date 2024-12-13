6b8379f9570417c5e2fabc8e8e94041f090fcf2f
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2024-12-13 10:58:53 +0700

    MARP-1633 Reformat code

--- cms-editor-test/src_test/com/axonivy/utils/cmseditor/test/CmsEditorWebTest.java
+++ cms-editor-test/src_test/com/axonivy/utils/cmseditor/test/CmsEditorWebTest.java
@@ -31,8 +31,8 @@ public class CmsEditorWebTest {
  * Please do not submit it as part of our bug bounty program.
  */
   void startProcess() {
-	open(EngineUrl.createProcessUrl(
-				"/cms-editor-test/193BDA54C9726ADF/logInUser.ivp?password=123456&username=testUser1"));
+    open(EngineUrl
+        .createProcessUrl("/cms-editor-test/193BDA54C9726ADF/logInUser.ivp?password=123456&username=testUser1"));
     open(EngineUrl.createProcessUrl("/cms-editor/18DE86A37D77D574/start.ivp?showEditorCms=true"));
   }
 
