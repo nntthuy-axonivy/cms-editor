8a0d46bfbc7e79c3b625b01c5e7ace548425d9f8
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2024-12-13 11:15:02 +0700

    MARP-1633 Handle Feedbacks

--- cms-editor-test/src_test/com/axonivy/utils/cmseditor/test/CmsEditorWebTest.java
+++ cms-editor-test/src_test/com/axonivy/utils/cmseditor/test/CmsEditorWebTest.java
@@ -138,7 +138,7 @@ public class CmsEditorWebTest {
   * This credential is intentionally included for educational purposes only and does not provide access to any production systems.
   * Please do not submit it as part of our bug bounty program.
   */
-  public void testUser_correctRole() {
+  public void testUserCorrectRole() {
     $(By.id("content-form:cancel-button")).shouldBe(visible).click();
     open(EngineUrl
         .createProcessUrl("/cms-editor-test/193BDA54C9726ADF/logInUser.ivp?password=123456&username=testUser1"));
@@ -153,13 +153,12 @@ public class CmsEditorWebTest {
   * This credential is intentionally included for educational purposes only and does not provide access to any production systems.
   * Please do not submit it as part of our bug bounty program.
   */
-  public void testUser_IncorrectRole() {
+  public void testUserIncorrectRole() {
     $(By.id("content-form:cancel-button")).shouldBe(visible).click();
     open(EngineUrl
         .createProcessUrl("/cms-editor-test/193BDA54C9726ADF/logInUser.ivp?password=123456&username=testUser2"));
     open(EngineUrl.createProcessUrl("/cms-editor/18DE86A37D77D574/start.ivp?showEditorCms=true"));
     var exception = $(By.cssSelector(".exception-content"));
-    Selenide.sleep(5000);
     exception.shouldBe(visible).shouldHave(matchText("Access denied. Need role CMS_ADMIN"));
   }
 }
